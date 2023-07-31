4e95c668e670bc10400ed3fca95f79cbfd091c5b
Jay <wnwoghd22@gmail.com>
2023-07-31 17:35:37 +0900

    Fix: Add multiple origins

--- back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://i9d203.p.ssafy.io:3000");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000",
+				"http://i9d203.p.ssafy.io:3000");
+	}
 }
