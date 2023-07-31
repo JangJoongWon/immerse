51062c55fc1bcb360737c6799d025f39be9c9910
주재홍 <wnwoghd22@gmail.com>
2023-07-31 17:17:28 +0900

    !BREAKING CHANGE: CORS 개방
    localhost를 임시로 열었습니다. 개발 완료 후 바로 닫아야 합니다.

--- back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
                 .allowedOrigins("http://i9d203.p.ssafy.io:3000");
     }
 }
