829bff93ae12ecbee643bb7ffd50be044cd11f3e
koobonjae <ninth6764@naver.com>
2023-08-01 15:02:47 +0900

    Feat 마이페이지 홈 컴포넌트 생성 마이페이지 홈 컴포넌트를 만들었습니다

--- front-end/src/components/mypagehome/MyPageHome.jsx
+++ front-end/src/components/mypagehome/MyPageHome.jsx
@@ -4,39 +4,59 @@
 import data from '../../stage_data.json';
 import guest from '../../guest.json';
 import StageCard from '../cards/stagecard';
-import {Row,Col} from 'react-bootstrap'
-import styles from "./MyPageHome.module.css"
-
-
+import { Row, Col } from 'react-bootstrap';
+import styles from './MyPageHome.module.css';
 
 function MyPageHome(props) {
-    console.log(props)
-    var {user_id} = props;
-    var stage_list = data.filter((stage) => {
+  var { user_id } = props;
+  var stage_list = data.filter((stage) => {
     return user_id === stage.fields.user_id;
-    });
-    var first_satge = stage_list.slice(0,1)
-    // var homeguestbook = guest.slice(0,4)
-    console.log(first_satge)
-    // console.log(homeguestbook)
-  return (
-    <div>MyPageHome
-        <Row>
-            <Col>
-            {/* <StageCard
-                className={styles.component}
-                data={first_stage} />  */}
-            </Col>
-            <Col>
-                {/* {homeguestbook.map((data)=>
-                (data.content))} */}
-            </Col>
-        </Row>
-        <Row>
+  });
 
-        </Row>
+  // Sort stage_list by date in descending order (most recent first)
+  stage_list.sort((a, b) => new Date(b.fields.date) - new Date(a.fields.date));
+
+  return (
+    <div
+    className={styles.container}>
+      <Row
+      className={styles.up}>
+        <Col>
+          <StageCard className={styles.component} data={stage_list[0]} />
+        </Col>
+        <Col
+         className={styles.homeguestbook}>
+          {guest.data.map((review) => (
+            <div 
+            className={styles.review} key={review.id}>
+              {review.content}
+            </div>
+          ))}
+        </Col>
+      </Row>
+      <Row>
+        <div 
+        className={styles.title}>
+            이전공연기록
+            <hr style={{color:"white"}}/>
+        </div>
+            
+        {/* Display previous stage records */}
+        <div
+        className={styles.bottom}>
+            <Row>
+            {stage_list.slice(1).map((stage) => (
+            <Col
+            className={styles.card} 
+            key={stage.id}>
+                <StageCard className={styles.component} data={stage} />
+            </Col>
+            ))}
+            </Row>
+        </div>
+      </Row>
     </div>
-  )
+  );
 }
 
-export default MyPageHome
+export default MyPageHome;
