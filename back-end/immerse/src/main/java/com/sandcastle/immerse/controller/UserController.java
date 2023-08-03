3f884044892491237d174cb7fa16a68c5153c97f
JangJoongWon <wnddnjs843@naver.com>
2023-08-03 14:03:46 +0900

    Feat: Add Search function

--- back-end/immerse/src/main/java/com/sandcastle/immerse/controller/UserController.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/controller/UserController.java
@@ -11,50 +11,50 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    private final UserService userServiceImpl;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody UserDto userDtoRequest) throws Exception {
-        UserDto userDtoResponse = userService.signup(userDtoRequest);
+        UserDto userDtoResponse = userServiceImpl.signup(userDtoRequest);
         return ResponseEntity.ok().body(userDtoResponse);
     }
 
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody UserDto userDtoRequest) throws Exception {
-        String token = userService.signin(userDtoRequest);
+        String token = userServiceImpl.signin(userDtoRequest);
         return ResponseEntity.ok().body(token);
     }
 
     @GetMapping("")
     public ResponseEntity<?> getMyUser(Authentication authentication) throws Exception {
         Long userId = Long.valueOf(authentication.getName());
-        UserDto userDtoResponse = userService.getMyUser(userId);
+        UserDto userDtoResponse = userServiceImpl.getMyUser(userId);
         return ResponseEntity.ok().body(userDtoResponse);
     }
 
     @GetMapping("/{nickname}")
     public ResponseEntity<?> getUser(@PathVariable String nickname) throws Exception {
-        UserDto userDtoResponse = userService.getUser(nickname);
+        UserDto userDtoResponse = userServiceImpl.getUser(nickname);
         return ResponseEntity.ok().body(userDtoResponse);
     }
 
     @GetMapping("/check/{nickname}")
     public ResponseEntity<?> existsByNickname(@PathVariable String nickname) throws Exception {
-        boolean check = userService.existsByNickname(nickname);
+        boolean check = userServiceImpl.existsByNickname(nickname);
         return ResponseEntity.ok().body(check);
     }
 
     @PutMapping("/update/info")
     public ResponseEntity<?> updateUser(@RequestBody UserDto userDtoRequest, Authentication authentication) throws Exception {
         Long userId = Long.valueOf(authentication.getName());
-        userService.updateUser(userId, userDtoRequest);
+        userServiceImpl.updateUser(userId, userDtoRequest);
         return ResponseEntity.ok().body("유저정보 수정완료");
     }
 
     @DeleteMapping("/withdrawal")
     public ResponseEntity<?> withdrawal(Authentication authentication) throws Exception {
         Long userId = Long.valueOf(authentication.getName());
-        int changedStatus = userService.withdrawal(userId);
+        int changedStatus = userServiceImpl.withdrawal(userId);
         return ResponseEntity.ok().body(changedStatus);
     }
 
