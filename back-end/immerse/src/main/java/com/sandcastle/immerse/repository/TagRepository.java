7b7534bd40709fdc2920a44dd97a4f2671dc4ac0
김현진 <hyunjin.k1994@gmail.com>
2023-08-04 10:30:20 +0900

    ADD tagRepository

--- back-end/immerse/src/main/java/com/sandcastle/immerse/repository/TagRepository.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/repository/TagRepository.java
@@ -1,7 +1,10 @@
 package com.sandcastle.immerse.repository;
 
+import com.sandcastle.immerse.model.dto.TagDto;
 import com.sandcastle.immerse.model.entity.TagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +12,16 @@ import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<TagEntity, Long> {
+
+    TagEntity save(TagDto tagDto);
+
     List<TagEntity> findAll();
 
     Optional<TagEntity> findById(Long id);
 
+    @Modifying
+    @Query(value = "UPDATE tags SET tag_name = ")
+    TagEntity update(Long id, TagDto tagDto);
+
+//    void delete(TagDto tagDto);
 }
