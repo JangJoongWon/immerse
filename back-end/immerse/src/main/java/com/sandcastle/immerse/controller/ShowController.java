8a17fccd97f5542b9fcf43dd5825e8ba2066338b
Jay <wnwoghd22@gmail.com>
2023-08-06 21:05:33 +0900

    HOTFIX: add log

--- back-end/immerse/src/main/java/com/sandcastle/immerse/controller/ShowController.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/controller/ShowController.java
@@ -93,6 +93,7 @@ public class ShowController {
 	@ResponseBody
 	@PutMapping("/{show_id}/start")
 	public ResponseEntity<?> startShow(@PathVariable Long show_id, Authentication auth) {
+		log.trace(auth.toString());
 		Long user_id = Long.valueOf(auth.getName());
 		log.trace("user: " + user_id);
 		log.trace("show: " + show_id);
