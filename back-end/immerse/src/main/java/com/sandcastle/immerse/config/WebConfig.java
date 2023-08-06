23c6de1492284d4a3175b7a4f69e088a3d9bb37a
Jay <wnwoghd22@gmail.com>
2023-08-06 18:41:32 +0900

    HOTFIX

--- back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
+++ back-end/immerse/src/main/java/com/sandcastle/immerse/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.sandcastle.immerse.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:3000",
 				"http://i9d203.p.ssafy.io:3000",
-				"https://i9d203.p.ssafy.io");
+				"https://i9d203.p.ssafy.io")
+			.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.HEAD.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.DELETE.name(),
+				HttpMethod.PUT.name());
 	}
 }
