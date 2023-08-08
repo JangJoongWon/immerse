7830484bc0f08f30753c1ba11113fc50c872947f
안영기 <thfl9281@naver.com>
2023-08-08 11:29:09 +0900

    Feat: AddRedirction

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,54 +1,55 @@
-// import React, { useState } from 'react'
-import Header from './components/header/Header'
-import styles from './App.module.css'
+import React from 'react';
+import Header from './components/header/Header';
+import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
-
-import Home from "./pages/home/Home";
-import SignIn from "./pages/sign/SignIn";
-// import SignOut from "./pages/sign/SignOut";
-// import Audience from './pages/stage/Audience';
-// import Performer from './pages/stage/Performer';
-import SignUp from "./pages/sign/SignUp";
-import Stage from "./pages/stage";
-import Mypage from "./pages/mypage/MyPage";
-import Search from "./pages/search/Search";
-import Category from "./pages/category/Category";
-import Checkpassword from "./pages/checkpassword/CheckPassword"
-import NotFound from "./pages/notfound/NotFound";
+import Home from './pages/home/Home';
+import SignIn from './pages/sign/SignIn';
+import SignUp from './pages/sign/SignUp';
+import Stage from './pages/stage';
+import Mypage from './pages/mypage/MyPage';
+import Search from './pages/search/Search';
+import Category from './pages/category/Category';
+import Checkpassword from './pages/checkpassword/CheckPassword';
+import NotFound from './pages/notfound/NotFound';
 import MyOption from './pages/myoption/MyOption';
-import Test from "./components/inputpicture/test";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CameraTest from "./pages/cameratest/CameraTest";
+import Test from './components/inputpicture/test';
+import CameraTest from './pages/cameratest/CameraTest';
 
 function App() {
+  const token = useSelector((state) => state.user.token);
 
   return (
-      <Router>
-        <div>
-          <div className={styles.header}>
-            <Header/>
-          </div>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            {/* <Route path="/stage" element={<Stage />} /> */}
-            {/* <Route path="/Audience" element={<Audience />} /> */}
-            {/* <Route path="/performer" element={<Performer />} /> */}
-            <Route path="/stage/:id" element={<Stage />} />
-            <Route path="/mypage/:nickname" element={<Mypage />} />
-            <Route path="/search/:word" element={<Search />} />
-            <Route path="/category/:id" element={<Category />} />
-            <Route path="/checkpassword" element={<Checkpassword />} />
-            <Route path="/myoption" element={<MyOption />} />
-            <Route path="/cameratest" element={<CameraTest />} />
-            <Route path="/*" element={<NotFound />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
+    <Router>
+      <div>
+        <div className={styles.header}>
+          <Header />
         </div>
-      </Router>
-  )
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          {!token && <Route path="/login" element={<SignIn />} />}
+          {token && <Route path="/login" element={<Navigate to="/" replace />} />}
+          {!token && <Route path="/signup" element={<SignUp />} />}
+          {token && <Route path="/signup" element={<Navigate to="/" replace />} />}
+
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+
+          <Route path="/stage/:id" element={<Stage />} />
+          <Route path="/mypage/:nickname" element={<Mypage />} />
+          <Route path="/search/:word" element={<Search />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/checkpassword" element={<Checkpassword />} />
+          <Route path="/myoption" element={<MyOption />} />
+          <Route path="/cameratest" element={<CameraTest />} />
+          <Route path="/*" element={<NotFound />} />
+          {/* <Route path="/test" element={<Test />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
