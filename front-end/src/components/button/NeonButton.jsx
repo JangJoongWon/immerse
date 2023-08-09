130a05f3298c5e667fe31cdaa1a4079ee6bde3cd
안영기 <thfl9281@naver.com>
2023-08-09 23:46:22 +0900

    Feat: AddUnloadLogOut/DesignChannel

---
+++ front-end/src/components/button/NeonButton.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './NeonButton.module.css'
+
+function NeonButton() {
+  return (
+    <div>
+        <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+    </a>
+    <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+    </a>
+    <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+    </a>
+    </div>
+  )
+}
+
+export default NeonButton
