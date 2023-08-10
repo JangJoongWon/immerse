4ee974946326eccc2732a0531cd0089bb376fd51
hyunjin.kim <hyunjin.k1994@gmail.com>
2023-08-10 23:09:41 +0900

    Feat : 기존 Test task 삭제, Jenkins 키값 할당 배포 확인

--- back-end/immerse/src/test/java/com/sandcastle/immerse/ImmerseApplicationTests.java
+++ back-end/immerse/src/test/java/com/sandcastle/immerse/ImmerseApplicationTests.java
@@ -12,26 +12,5 @@ class ImmerseApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	@Value("${jasypt.encryptor.password")
-	private String key;
-	private String algorithm = "PBEWithMD5AndDES";
-
-	@Test
-	public void testEncryptionKey(){
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(key);
-		config.setAlgorithm(algorithm);
-		config.setKeyObtentionIterations("1000");
-		config.setPoolSize("1");
-		config.setProviderName("SunJCE");
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-		config.setStringOutputType("base64");
-		encryptor.setConfig(config);
-
-
-		String plaintext = "root";
-		System.out.println(plaintext + " ->  " +encryptor.encrypt(plaintext));
-	}
 
 }
